c576aaacb1d280cc0875a7228fe020188d014f82
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-09-15 08:18:39 -0600

    Session creation sad path, no flash message yet
    Co-authored-by: Wil <Wrfady@gmail.com>
    Co-authored-by: Chris <chris.asercion@gmail.com>'

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # describe 'validations' do 
+  #   it { should have_secure_password }
+  #   # it {should validate_presence_of(:email) }
+  #   # it {should validate_presence_of :password_digest}
+  #   # it {should validate_uniqueness_of :email}
+  # end
 end
