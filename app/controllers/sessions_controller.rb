b0ca59714e6f10489d1fe7bf4bc054225b858113
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-09-14 20:04:04 -0600

    Session creation
    Co-authored-by: Wil <Wrfady@gmail.com>
    Co-authored-by: Chris <chris.asercion@gmail.com>'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create 
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to spotify_oa_path
+    else
+      flash[:error] = "Your email or password was incorrect."
+      redirect_to login_path
+    end
+  end
 end
--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -3,11 +3,22 @@ require "rails_helper"
 RSpec.describe "User Login", type: :feature do
   describe "traditional user login" do
     it "logs a user in with valid credentials" do
+
+      user = User.create!(email: "doot@doot.com", password: "test123")
+
       visit root_path
 
       click_on "Log in"
 
       expect(current_path).to eq(login_path)
+
+      fill_in "Email", with: "doot@doot.com"
+      fill_in "Password", with: "test123"
+      click_on "Log In"
+
+      expect(current_path).to eq(spotify_oa_path)
+      expect(page).to have_link('Sign in with Spotify')
+      #Do I need to add a Logout for the OAuth page?
     end
   end
 end
