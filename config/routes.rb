468976748624cc1b8c3b085a8add2f6405e4ea72
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-09-18 15:13:41 -0600

    Light/Dark mode persists page to page after  being initially set
    Co-authored-by: Wil <Wrfady@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "welcome#index"
 
-  # get "/login", to: "sessions#new"
-  # post "/login", to: "sessions#create"
-
-  # get "/auth", to: "sessions#create"
+  get "/auth", to: "sessions#create"
 
   get "/login", to: "users#new"
   post "/login", to: "users#create"
