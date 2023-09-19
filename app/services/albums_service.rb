ad91d3bbeeec9b6b30227667b3939cefcbd67e79
Chris Asercion <chris.asercion@gmail.com>
2023-09-19 12:00:43 -0600

    album service spec

--- app/services/albums_service.rb
+++ app/services/albums_service.rb
@@ -3,9 +3,9 @@ class AlbumsService
     Faraday.new(url: "http://localhost:3000/api/v1/#{endpoint_url}")
   end
 
-  def self.find_albums(album)
+  def self.find_albums(albums)
     response = conn('search_album').get do |faraday| 
-      faraday.params['album'] = album
+      faraday.params['album'] = albums
     end
     JSON.parse(response.body, symbolize_names: true)
   end
---
+++ spec/facades/albums_service.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe AlbumsFacade do 
+  describe "class methods" do
+    describe "" do
+      
+    end
+  end
+end
