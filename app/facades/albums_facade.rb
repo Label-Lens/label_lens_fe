f33b457e8a4c036c3117c8533a000fac141eeeb1
Davis Weimer <davisweimer@gmail.com>
2023-09-21 10:07:14 -0600

    styling: album search draft
    Co-authored-by: andilovetto <andilovetto@users.noreply.github.com>

--- app/facades/albums_facade.rb
+++ app/facades/albums_facade.rb
@@ -1,7 +1,6 @@
 class AlbumsFacade
   def self.get_albums(album, artist)
     albums = AlbumsService.find_albums(album, artist)
-    
     albums[:data].map do |album|
       Album.new(album)
     end
--- spec/features/spotify/search.rb
+++ spec/features/spotify/search.rb
@@ -2,11 +2,18 @@ require "rails_helper"
 
 RSpec.describe "Spotify  Search", type: :feature do
   describe 'Sporify Search function' do
-    scenario 'A search bar to type in the name of an album' do
+    scenario 'A search bar to type in the name of an album', :vcr do
       visit spotify_search_path
 
-      expect(page).to have_field('query')
-      
+      expect(page).to have_field('Album...')
+      expect(page).to have_field('Artist...')
+      fill_in "album", with: "Leprosy"
+      fill_in "artist", with: "Death"
+      click_button "Search"
+
+      expect(current_path).to eq(albums_path)
+      # require 'pry'; binding.pry
+      save_and_open_page
     end
   end
 end
