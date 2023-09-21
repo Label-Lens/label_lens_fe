9a26aecbd1d9f7ad87eff6a5faab3ad1aa4c788c
Wil Fady <wrfady@gmail.com>
2023-09-20 20:37:34 -0400

    setup for fe deployment

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,9 @@
 class AlbumsController < ApplicationController
+  ALBUMS_PER_PAGE = 3
+  
   def index
-    @albums = AlbumsFacade.get_albums(params[:query])
-
+    # @current_page = params[:page] || 1
+    @albums = AlbumsFacade.get_albums(params[:query]) 
+    # require 'pry'; binding.pry
   end
 end
